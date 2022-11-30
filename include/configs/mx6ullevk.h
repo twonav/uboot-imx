d03f7678fa2fd6e4ee42592cae93702780f062a2
Theodoros Paschidis <tpaschidis@twonav.com>
2022-11-30 15:43:59 +0100

    TWON-18351: Uboot for Roc

--- include/configs/mx6ullevk.h
+++ include/configs/mx6ullevk.h
@@ -14,7 +14,7 @@
 #include "mx6_common.h"
 #include <asm/imx-common/gpio.h>
 
-#define UBOOT_VERSION "v1.2.8"
+#define UBOOT_VERSION "v1.2.9"
 #define CONFIG_VERSION_VARIABLE
 
 /* uncomment for PLUGIN mode support */
